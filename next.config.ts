b6779aec1ad30a50a1e75f73053ba493498299eb
Br-iON <26982008+KaliStudent@users.noreply.github.com>
2025-09-16 03:58:10 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,6 @@
 import type { NextConfig } from 'next';
-
+import dotenvExpand from 'dotenv-expand';
+import { loadEnv, defineConfig } from 'vite';
 const nextConfig: NextConfig = {
   experimental: {
     ppr: true,
@@ -8,9 +9,21 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         hostname: 'avatar.vercel.sh',
+        
       },
     ],
   },
+export default defineConfig(({ mode }) => {
+  // This check is important!
+  if (mode === 'development') {
+    const env = loadEnv(mode, process.cwd(), '');
+    dotenvExpand.expand({ parsed: env });
+  }
+
+  return {
+    ...
+  };
+});
 };
 
 export default nextConfig;
